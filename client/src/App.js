dcdedcc0801f5a45108fc278d4cd897db00588c5
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-04-26 11:29:51 -0400

    importing styles for heading and picture

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import puppy from './images/goldenretriever.jpg';
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Four Paw's Best Buddies</Typography>
-                <img src={puppy} alt="dog" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Four Paw's Best Buddies</Typography>
+                <img className={classes.image} src={puppy} alt="dog" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
