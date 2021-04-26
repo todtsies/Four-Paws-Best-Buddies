96cd598e4be3b0c89a613f4b093c2874456d1083
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-04-26 12:53:48 -0400

    Creating form for input from users

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useStat } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 
 import useStyles from './styles';
@@ -6,8 +6,25 @@ import useStyles from './styles';
 const Form = () => {
     const classes = useStyles();
     
+    const handleSubmit = () => {
+
+    }
+    
     return (
-        <h1>FORM</h1>
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+            <Typography variant="h6">Add your Pup</Typography>
+            <TextField 
+                name="creator" 
+                variant="outlined" 
+                label="Your Name" 
+                fullWidth
+                value={postData.creator}
+                onChange={}
+            />
+            </form>
+            
+        </Paper>
     );
 }
 
