3755681b490ee96cf57db33ab8af5402263fa000
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-05-03 13:11:52 -0400

    Update post functionality added

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,45 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper, MenuItem } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import useStyles from './styles';
-import { createPost } from '../../actions/posts';
+import { createPost, updatePost } from '../../actions/posts';
 
 const services = [
     {
-        value: 'daycare',
+        value: 'Daycare',
         label: 'Daycare',
       },
       {
-        value: 'boarding',
+        value: 'Boarding',
         label: 'Boarding',
       },
       {
-        value: 'grooming',
+        value: 'Grooming',
         label: 'Grooming',
       },
 ];
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({ creator: '', dogsName: '', message: '', breed: '', service: '', selectedFile: '' });
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(post) setPostData(post);
+    }, [post])
     
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("HI", e);
-        dispatch(createPost(postData));
+
+        if(currentId) {
+            dispatch(updatePost(currentId, postData));
+        } else {
+            dispatch(createPost(postData));
+
+        }
+        
+        clear();
     }
     
     const clear = () => {
-
+        setCurrentId(null);
+        setPostData({ creator: '', dogsName: '', message: '', breed: '', service: '', selectedFile: '' })
     }
 
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant="h6">Add your Pup</Typography>
+                <Typography variant="h6">{ currentId ? 'Edit' : 'Add'} your Pup</Typography>
                 
                 <TextField name="creator" variant="outlined" label="Your Name" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 
