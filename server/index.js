28ebdae0658d03feb707d574856794bec14f1712
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-05-04 14:11:53 -0400

    updated URL

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js'
 
 const app = express();
-dotenv.config();
+// dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -17,7 +17,9 @@ app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const CONNECTION_URL = 'mongodb+srv://todtsies:Colton123@cluster0.0brkr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
