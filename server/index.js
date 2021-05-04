f0856a32b9f46059aad0f16f27351cabf9413ed0
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-05-04 15:20:27 -0400

    destructured

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import path from 'path';
 // import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js'
-import { MONGODB_URI } from './config/keys.js';
+import MONGODB_URI from './config/keys.js';
 
 const app = express();
 // dotenv.config();
