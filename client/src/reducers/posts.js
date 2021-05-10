1718f297e6cf9e9645195aa2fcaa7a7b2736119c
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-05-10 15:42:48 -0400

    Update posts.js

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,19 +1,27 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import { FETCH_ALL, FETCH_BY_SEARCH, FETCH_POST, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes.js';
 
-export default (posts = [], action) => {
+export default (posts = { isLoading: true, posts: [] }, action) => {
     switch (action.type) {
-        case DELETE:
-            return posts.filter((post) => post._id !== action.payload);
-        case UPDATE:
-            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
-        case LIKE:
-            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+        case 'START_LOADING':
+            return { ...posts, isLoading: true };
+        case 'END_LOADING':
+            return { ...posts, isLoading: false };
         case FETCH_ALL:
-            return action.payload;
+            return { ...posts, posts: action.payload.data };
+        case FETCH_BY_SEARCH:
+            return { ...posts, posts: action.payload.data };
+        case FETCH_POST:
+            return { ...posts, post: action.payload.post };
+        case LIKE:
+            return { ...posts, posts: posts.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
         case CREATE:
-            return [...posts, action.payload];
+            return { ...posts, posts: [...posts.posts, action.payload] };
+        case UPDATE:
+            return { ...posts, posts: posts.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
+        case DELETE:
+            return { ...posts, posts: posts.posts.filter((post) => post._id !== action.payload) };
         default:
-            return posts;
+            return { ...posts, posts };
     }
 }
