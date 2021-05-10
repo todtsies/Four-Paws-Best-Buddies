76755d5e842628172bccd3539e06141bc544754a
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-05-10 15:05:06 -0400

    Update posts.js
    add getPostsBySearch function and getPost function

--- controllers/posts.js
+++ controllers/posts.js
@@ -16,29 +16,56 @@ export const getPosts = async (req, res) => {
     }
 }
 
+export const getPostsBySearch = async (req, res) => {
+    const { searchQuery } = req.query;
+
+    try {
+        const dogsName = new RegExp(searchQuery, "i");
+    
+        const posts = await PostMessage.find({ dogsName });
+
+        res.json({ data: posts });
+    } catch (error) {    
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getPost = async (req, res) => { 
+    const { id } = req.params;
+
+    try {
+        const post = await PostMessage.findById(id);
+        
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
 
 export const createPost = async (req, res) => {
     const post = req.body;
 
-    const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
+    const newPostMessage = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
     
     try {
-        await newPost.save();
+        await newPostMessage.save();
 
-        res.status(201).json(newPost);
+        res.status(201).json(newPostMessage);
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
 }
 
 export const updatePost = async (req, res) => {
-    const { id: _id } = req.params;
-    const post = req.body;
-
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID');
-
+    const { id } = req.params;
+    const { dogsName, message, creator, selectedFile, breed } = req.body;
     
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ... post, _id }, { new: true });
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = { creator, dogsName, message, breed, selectedFile, _id: id };
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
 
     res.json(updatedPost);
 }
@@ -57,8 +84,10 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
     const { id } = req.params;
 
-    if(!req.userId) return res.json({ message: 'Unauthenticated'});
-
+    if(!req.userId) {
+        return res.json({ message: 'Unauthenticated'});
+    }
+    
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that ID');
 
     const post = await PostMessage.findById(id);
