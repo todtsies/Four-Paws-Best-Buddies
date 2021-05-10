8f1f09e8829a17b3fd911ad8420f0ba7e3f62e74
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-05-10 11:47:07 -0400

    still deploying...

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import cors from 'cors';
-
 import { MONGODB_URI } from './config/dev.js';
 import path from 'path';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
 
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
@@ -15,17 +13,16 @@ import userRoutes from './routes/users.js';
 const app = express();
 
 
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+// app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = dirname(fileURLToPath(import.meta.url)); 
+// const __dirname = dirname(fileURLToPath(import.meta.url)); 
 
 if (process.env.NODE_ENV === "production") {
   mongoose.connect(MONGODB_URI,
@@ -38,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
       mongoose.connection.on('error',(err)=>{
           console.log('err connecting', err)
       })
-      mongoose.set('useFindAndModify', false);
+      // mongoose.set('useFindAndModify', false);
       
       app.use(express.static("client/build"));
       app.get('*', (req, res) => {
