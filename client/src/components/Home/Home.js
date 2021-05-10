8e63a116246718d5e7865364cf6563fc2a01986d
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-05-10 16:50:04 -0400

    added search bar to home component and styling accordingly

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,37 +1,62 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, AppBar, TextField, Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-import { getPosts } from '../../actions/posts';
+
+import { useHistory } from 'react-router-dom';
+
+import { getPosts, getPostsBySearch } from '../../actions/posts.js';
 import useStyles from './styles';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 
 
 const Home = () => {
+    const classes = useStyles();
     const [currentId, setCurrentId] = useState(0);
     const dispatch = useDispatch();
-    const classes = useStyles();
-    
+
+    const [search, setSearch] = useState('');
+    const history = useHistory();
+
 
     useEffect(() => {
         dispatch(getPosts());
     }, [currentId, dispatch]);
 
+    const searchPost = () => {
+        if (search.trim()) {
+            dispatch(getPostsBySearch(search));
+            history.push(`/posts/search?searchQuery=${search}`);
+        } else {
+            history.push('/');
+        }
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.keyCode === 13) {
+            searchPost();
+        }
+    };
+
 
     return (
         <Grow in>
-            <Container>
-                <Grid className={classes.direction} container justify="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
+            <Container maxWidth="xl">
+                <Grid container justify="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
+                    <Grid item xs={12} sm={7} >
                         <Posts setCurrentId={setCurrentId} />
                     </Grid>
-                    <Grid item xs={12} sm={4}>
+                    <Grid item xs={12} sm={4} >
+                        <AppBar className={classes.appBarSearch} position="static" color="inherit">
+                            <TextField onKeyDown={handleKeyPress} name="search" variant="outlined" label="Search by dog's name" fullWidth value={search} onChange={(e) => setSearch(e.target.value)} />
+                            <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
+                        </AppBar>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
                     </Grid>
                 </Grid>
             </Container>
         </Grow>
     );
-}
+};
 
 export default Home;
--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -1,7 +1,18 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-    [theme.breakpoints.down('sm')]: {
-        direction: "column-reverse",
+    appBarSearch: {
+        borderRadius: 4,
+        marginBottom: '1rem',
+        display: 'flex',
+        padding: '16px',
     },
-}))
+    searchButton: {
+        marginTop: '5px',
+    },
+    gridContainer: {
+        [theme.breakpoints.down('xs')]: {
+            flexDirection: 'column-reverse',
+        },
+    },
+}));
